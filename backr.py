8ab9785eadf8a296e57435200680bc058a4d47a6
Anthony Fadly <18fadly.anthony@gmail.com>
2018-04-25 09:05:17 -0700

    compress files not in folder

--- backr.py
+++ backr.py
@@ -131,7 +131,7 @@ if use_compression:
     def make_tarfile(output_filename, source_dir):
         with tarfile.open(output_filename, "w:gz") as tar:
             tar.add(source_dir, arcname=os.path.basename(source_dir))
-    make_tarfile(backup_location+"/test.tar.gz",backup_location)
+    make_tarfile(backbase+"/"+time+".tar.gz",backup_location)
 
 print "folder backed up to "+backup_location
 
