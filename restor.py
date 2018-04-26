b3a020d11c55eb84dd01d16515ecd9e9788ac25f
Anthony Fadly <18fadly.anthony@gmail.com>
2018-04-26 09:11:43 -0700

    restor now detects backr-location

--- restor.py
+++ restor.py
@@ -2,12 +2,26 @@
 
 # backr restor tool in python
 
-import sys
-
-def main()
+import sys, os
 
 for i in range(len(sys.argv)):
     if "-h" in sys.argv or "--help" in sys.argv:
        print "restor tool for backr"
        print "usage: backr.py [-h|--help]"
        exit()
+
+def main():
+    if os.path.isfile(".backr-location"):
+        with open('.backr-location', 'r') as myfile:
+            backup_location = myfile.read()
+    else:
+        print ".backr-location file not found"
+        sys.exit(0)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print
+        print "exit"
+        sys.exit(0)
