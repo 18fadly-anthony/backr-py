35edf234824237d49c6ac1437514ec8c3237e590
Anthony Fadly <18fadly.anthony@gmail.com>
2019-01-28 13:43:37 -0800

    fix import, whitespace in restor.py

--- restor.py
+++ restor.py
@@ -2,21 +2,24 @@
 
 # backr restor tool in python
 
-import sys, os, hashlib, shutil
+import sys
+import os
+import hashlib
+import shutil
 from distutils.dir_util import copy_tree
 import cPickle as pickle
 
 for i in range(len(sys.argv)):
     if "-h" in sys.argv or "--help" in sys.argv:
-       print "restor tool for backr"
-       print "usage: restor.py [-h|--help]"
-       print "[-h|--help] - print this help"
-       exit()
+        print "restor tool for backr"
+        print "usage: restor.py [-h|--help]"
+        print "[-h|--help] - print this help"
+        exit()
 
 def main():
     # set veriables
-    dir=os.getcwd()
-    basename=os.path.basename(dir)
+    dir = os.getcwd()
+    basename = os.path.basename(dir)
 
     # get backup location
     if os.path.isfile(".backr-location"):
@@ -35,24 +38,24 @@ def main():
 
     backup_location += "/"
     backup_location += basehash
-    backbase=backup_location
+    backbase = backup_location
 
-    vc_file=backbase+"/backtrack.txt"
+    vc_file = backbase+"/backtrack.txt"
 
     print "listing backups..."
     print
     if os.path.exists(vc_file):
-        data=pickle.load( open( vc_file, "rb" ))
-        possible_backups=["placeholder"]
+        data = pickle.load(open(vc_file, "rb"))
+        possible_backups = ["placeholder"]
         for i in range(0, len(data)):
             print "backup "+str(i)+" in "+data[i]
             if not os.path.exists(data[i]):
-                print ("    backup deleted or missing!")
+                print "    backup deleted or missing!"
             else:
-                if possible_backups==["placeholder"]:
-                    possible_backups=[i]
+                if possible_backups == ["placeholder"]:
+                    possible_backups = [i]
                 else:
-                    possible_backups+=[i]
+                    possible_backups += [i]
 
         # print most recent available backup
         print
@@ -60,13 +63,13 @@ def main():
         print data[possible_backups[-1]]
         print
 
-        has_number=False
+        has_number = False
         while not has_number:
             try:
-                backup_number=raw_input("Choose a number to restore from: ")
+                backup_number = raw_input("Choose a number to restore from: ")
                 if backup_number in str(possible_backups):
-                    backup_number=int(backup_number)
-                    has_number=True
+                    backup_number = int(backup_number)
+                    has_number = True
                 else:
                     print "That is not an option."
             except ValueError:
@@ -82,12 +85,12 @@ def main():
         restore_location = raw_input("Enter a location to restore to: ")
         if os.path.isdir(restore_location):
             print "Will restore to "+restore_location
-            has_restore_location=True
+            has_restore_location = True
         else:
             print restore_location+" does not exist"
 
-    restore_location+="/"
-    restore_location+=basename
+    restore_location += "/"
+    restore_location += basename
     if not os.path.exists(restore_location):
         os.makedirs(restore_location)
     if not ".tar.gz" in restore_from:
