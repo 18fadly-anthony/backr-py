1b0d281a9387b342d17a390638718017a6535de3
Anthony Fadly <18fadly.anthony@gmail.com>
2019-08-16 22:29:49 -0700

    create .backr-location even when -d flag is passed

--- backr.py
+++ backr.py
@@ -98,19 +98,19 @@ def main():
             if backup_location == "":
                 backup_location = default_location
 
-            # If the user-provided location exists, write it to the file
-            if os.path.isdir(backup_location):
-                f = open('.backr-location', 'w')
-                f.write(backup_location)
-                f.close()
-            else:
+        # If the user-provided location exists, write it to the file
+        if os.path.isdir(backup_location):
+            f = open('.backr-location', 'w')
+            f.write(backup_location)
+            f.close()
+        else:
 
-                # Create the folder only if it is ~/backrs
-                if backup_location == default_location:
-                    os.makedirs(backup_location)
-                else:
-                    print(backup_location + " does not exist")
-                    sys.exit(1)
+             # Create the folder only if it is ~/backrs
+             if backup_location == default_location:
+                 os.makedirs(backup_location)
+             else:
+                 print(backup_location + " does not exist")
+                 sys.exit(1)
 
     # Output
     print("will save to "+backup_location)
